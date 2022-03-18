87087c2c4fb0ed3e364861ba16cbea4553bd162a
Wagner Andrade <wagnerandrade_df@hotmail.com>
2022-03-18 18:58:31 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ![Web 1](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPb_LpFrUtpiedrq8VCkPKcVWvxFz1b9q4bg&usqp=CAU) Treinando React
+
+# Projeto em React - Mostrar repositórios dos usuários do GITHUB
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+
+function App(props) {
+  
+   return (
+    <>
+      <input name="usuario" id="usuario" className="usuarioInput" placeholder="Usuário" />
+      <button type="button">Pesquisar</button>
+    </>
+  );
+}
+
+export default App;
