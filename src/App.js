ea797e244b0b660e0050939bb45c7c5d49d9e303
Wagner Andrade <wagnerandrade_df@hotmail.com>
2022-03-20 13:15:45 -0300

    Consumindo dados de uma API via axios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App(props) {
+  const [ usuario, setUsuario ] = useState(''); 
   
-   return (
-    <>
-      <input name="usuario" id="usuario" className="usuarioInput" placeholder="Usuário" />
-      <button type="button">Pesquisar</button>
+  function handlePesquisa(){
+    axios.get(`http://api.github.com/users/${usuario}/repos`).then(response => console.log(response.data));  
+  }
+  return ( 
+    <> 
+      <input className="usuarioInput" placeholder="Usuário" value={usuario} onChange={e => setUsuario(e.target.value) }/>
+      <button type="button" onClick={handlePesquisa}>Pesquisar</button>
     </>
   );
 }
-
 export default App;
